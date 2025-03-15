d995de6e3ed6ce67bd161757bc316206c15178f2
Pranil Swami <100079926+pranilswami@users.noreply.github.com>
2025-03-15 21:04:33 +0530

    Update Test5Application.java

--- src/main/java/com/test5/test5/Test5Application.java
+++ src/main/java/com/test5/test5/Test5Application.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test5Application {
-
+	int x=10;
+	String name="mike"
+	boolean status=true;
+	float value=12.312
+	double value1=123.12331231
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
 	}
