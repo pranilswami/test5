defc4abb7047cf1516d635cc1cbcf0e7834fc327
Pranil Swami <100079926+pranilswami@users.noreply.github.com>
2025-03-15 21:07:01 +0530

    Update Test5Application.java

--- src/main/java/com/test5/test5/Test5Application.java
+++ src/main/java/com/test5/test5/Test5Application.java
@@ -8,8 +8,9 @@ public class Test5Application {
 	int x=10;
 	String name="mike"
 	boolean status=true;
-	float value=12.312
-	double value1=123.12331231
+	float value=12.312;
+	double value1=123.12331231;
+	char ch='x';
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
 	}
