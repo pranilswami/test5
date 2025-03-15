b02f009a63230aa6a559753a1a4bd480d99e2874
pranilswami <swamipranil@gmail.com>
2025-03-15 22:54:01 +0530

    bug-fix changes

--- src/main/java/com/test5/test5/Test5Application.java
+++ src/main/java/com/test5/test5/Test5Application.java
@@ -13,6 +13,7 @@ public class Test5Application {
 	char ch='x';
 	boolean isSub=true;
 	char ch1='n';
+	String b1 = "Changes in bug-fix";
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
 	}
